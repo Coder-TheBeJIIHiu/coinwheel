61d416213d69a3f29e61a26e272fe3533852c876
Coder-TheBeJIIHiu <83647537+Coder-TheBeJIIHiu@users.noreply.github.com>
2022-01-13 13:22:15 +0600

    Delete cmds/private directory

--- cmds/private/getConversation.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  tag: ["поиск беседы"],
-  button: ["getConversation"],
-  func: async(context, { vk, _user, game }) => {
-    let _conv = [`https://vk.me/join/AJQ1dzrOZxYHqMrKOm5dhuno`];
-    await context.send(`${_user.name}, ссылка на беседу - ${_conv[Math.floor(Math.random() * _conv.length)]}`);
-  }
-};
